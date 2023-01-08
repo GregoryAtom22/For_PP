41337da0bb4e324f28f1f819811f1f622579a287
grego <gregoryatom22@mail.ru>
2023-01-08 18:54:25 +0600

    Buy component created

--- src/hello.js
+++ src/hello.js
@@ -3,10 +3,31 @@ import React, { Component } from "react";
 export default class Hello extends Component {
     constructor() {
         super();
-        this.state = { color: "red"};
+        this.state = { color: "red", digit: 0}; //состояние 
     }
 
+    componentDidMount(){
+        console.log(this.props.parent_color)
+        setTimeout(()=> {
+            this.setState({ digit: 10, color: this.props.parent_color });
+        }, 2000);
+        console.log("componentDidMount");
+    }
+
+
+
     render(){
-        return <div style={{ backgroundColor: "green" }}>Hello</div>
+        console.log("render");
+        return 
+        <div>
+            <div onClick={()=>{
+                //this.state = { color: "yellow" } неработает
+                this.setState({ color: "yellow" });
+            }} style={{ backgroundColor: this.state.color }}
+            >
+                Hello
+            </div>
+            <div>[this.state.digit]</div>
+        </div>
     }
 }
